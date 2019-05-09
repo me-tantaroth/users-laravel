9db7b248ba799b922c3eb4e8b9f5ccec724b59ab
Eduard Ramirez <tantaroth@gmail.com>
2019-05-08 22:40:23 -0500

    Update text to spanish lang

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,12 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifica tu correo electronico') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Se ha enviado un email de verficacion a tu correo.') }}
                         </div>
                     @endif
 
